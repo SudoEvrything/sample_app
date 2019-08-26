521d77e613b891f766a103cc5e69dc7c9c065832
Runs <jetherruns@gmail.com>
2019-08-26 18:31:12 +0530

    Basic user model(including secure password using bcrypt)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@ require_relative '../config/environment'
 require 'rails/test_help'
 require "minitest/reporters"
 Minitest::Reporters.use!
+ActiveRecord::Migration.maintain_test_schema!
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
