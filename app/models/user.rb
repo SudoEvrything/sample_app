486a51f414cbfc68b9c14fb6ad10e82f9eda01b5
Runs <jetherruns@gmail.com>
2019-08-30 03:29:48 +0530

    account activation smtp configuration pending

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-	attr_accessor :remember_token
+	attr_accessor :remember_token, :activation_token
 
-	before_save { self.email = email.downcase }
+	before_save :downcase_email
+	before_create :create_activation_digest
 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
@@ -25,12 +26,32 @@ class User < ApplicationRecord
 		update_attribute(:remember_digest, User.digest(remember_token))
 	end
 
-	def authenticated?(remember_token)
-		return false if remember_digest.nil?
-		BCrypt::Password.new(remember_digest).is_password?(remember_token)		
-	end
-
 	def forget
 		update_attribute(:remember_digest, nil)
+	end
+
+	def authenticated?(attribute, token)
+		digest = send("#{attribute}_digest")
+		return false if digest.nil?
+		BCrypt::Password.new(digest).is_password?(token)		
+	end
+
+	def activate
+		update_columns(activated: true, activated_at: Time.zone.now)
+	end
+
+	def send_activation_email
+		UserMailer.account_activation(self).deliver_now
+	end
+
+	private
+
+	def downcase_email
+		self.email = email.downcase
+	end
+
+	def create_activation_digest
+		self.activation_token = User.new_token
+		self.activation_digest = User.digest(activation_token)
 	end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+
+	def setup
+		ActionMailer::Base.deliveries.clear
+	end
   
 	test "invalid signup information" do
 		get signup_path
@@ -19,6 +23,22 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 				user: { name: "Example rlj", email: "user@example.com", password: "password", password_confirmation: "password" }
 			}
 		end
+		assert_equal 1, ActionMailer::Base.deliveries.size
+		user = assigns(:user)
+		assert_not user.activated?
+
+		#Try to log in before activation
+		log_in_as(user)
+		assert_not is_logged_in?
+		#invalid activation token
+		get edit_account_activation_path("invalid token", email: user_email)
+		assert_not is_logged_in?
+		#Valid token wrong email
+		get edit_account_activation_path(user.activation_token email: 'wrong')
+
+		#valid activation token
+		get edit_account_activation_path(user.activation_token, email: user.email)
+		assert user.reload.activated?
 		follow_redirect!
 		assert_template 'users/show'
 		assert is_logged_in?
