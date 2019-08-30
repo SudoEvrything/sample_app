83ba77c4736550cb8b093c508ac4b5b016b305a1
Runs <jetherruns@gmail.com>
2019-08-30 11:43:55 +0530

    gmail smtp server

--- Gemfile
+++ Gemfile
@@ -17,6 +17,8 @@ gem 'jquery-rails', '~> 4.3.5'
 gem 'faker', '~>2.2.0'
 gem 'will_paginate', '~>3.1.8'
 gem 'bootstrap-will_paginate', '~>1.0.0'
+
+gem 'figaro', '~>1.1.1'
 # Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
 gem 'webpacker', '~> 4.0'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,11 +60,16 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "sample_app_production"
 
+  config.action_mailer.default_url_optins = { :host => 'lungkungakao.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = { address: "smtp.gmail.com", port: 465, domain: "lungkungakao.herokuapp.com", authentication: "plain", enable_starttls_auto: true, user_name: ENV["GMAIL_USERNAME"], password: ENV["GMAIL_PASSWORD"]}
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
